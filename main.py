43bcc619871c2e04fb620a5ea85134724ffd71fe
Miroshima <miro.vannini@protonmail.ch>
2022-09-29 09:39:34 +0200

    get video link

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # pip install requests
 import requests
+# pip install beautifulsoup4
+from bs4 import BeautifulSoup
 
-seriesURL = str(input("Please enter URL of a series: \n"))
+archive_url = str(input("Please enter URL of a series: \n"))
+
+def get_video_link(url):
+
+    # create response element
+    response = requests.get(url)
+
+    # create beautiful-soup object
+    soup = BeautifulSoup(response.content, "html5lib")
+
+    print(soup)
+
 
 def openBrowser(url):
     pass
+
+
+get_video_link(archive_url)

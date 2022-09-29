265c6433030145604f0bc8d8b18f08a65a2686bc
Miroshima <miro.vannini@protonmail.ch>
2022-09-29 14:21:05 +0200

    get season, video links

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 # pip install requests
+import re
+from turtle import title
 import requests
 # pip install beautifulsoup4
 from bs4 import BeautifulSoup
+# pip install html5lib
+import html5lib
 
-archive_url = str(input("Please enter URL of a series: \n"))
+base_url = "https://bs.to/"
 
-def get_video_link(url):
+def get_soup(url):
 
     # create response element
     response = requests.get(url)
 
     # create beautiful-soup object
-    soup = BeautifulSoup(response.content, "html5lib")
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    print(soup)
+    return soup
 
+def get_season_links(soup):
 
-def openBrowser(url):
-    pass
+    # get div with all seasons
+    seasons_div = soup.find("div", id="seasons")
 
+    # extract href from a
+    season_links = [base_url + link.get("href") for link in seasons_div.find_all("a")]
 
-get_video_link(archive_url)
+    return season_links
+
+def get_video_links(soup):
+
+    # get all href of Vidoza Vide
+
+    video_links = [base_url + link.get("href") for link in soup.find_all("a", title="Vidoza")]
+
+    return video_links
+
+if __name__ == "__main__":
+
+    soup = get_soup(str(input("Please enter URL of a series: \n")))
+
+    # get all seasons href
+    season_links = get_season_links(soup)
+
+    # getting all video links from a season
+    video_links = get_video_links(soup)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # for i in range(1, 10):
+    #     print(i)
+    #     for link in soup.find_all("li", class_="s"+str(i)):
+    #         print(link.get("href"))
